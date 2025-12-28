acf09dc5c4683e743bd093988a398bec0e3b6e0c
Mr I am <i@am.com>
2025-12-28 01:52:32 +0100

    Fix(admin): set base path to /admin-panel/ for correct asset loading

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    base: '/admin-panel/',
     server: {
         port: 5174, // Different port from client
         proxy: {
